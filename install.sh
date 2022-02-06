f33f6b869eadc5c10afe07dff1260e4493f52820
souovan <souovan@protonmail.com>
2022-02-05 21:07:24 -0300

    Testing upgrades to execute script directly from curl

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ fi
 
 if [[ $(cat /etc/issue) == *"Debian"* ]]; then
   if ! type vim &> /dev/null || ! type dconf &> /dev/null || ! type nvim &> /dev/null; then
-    echo "Instalando dependências"
+    echo "Digite a senha e aguarde a instalação das dependências"
     su -c "xargs -a apt_packages.txt apt-get install -y" &> /dev/null
   else
     printf "%s ~ vim e dconf-cli e nvim já estão instalados%s\n" "$YELLOW" "$NORMAL"
@@ -39,7 +39,7 @@ if [[ $(cat /etc/issue) == *"Debian"* ]]; then
 
 elif  [[ $(cat /etc/issue) == *"Ubuntu"* ]]; then 
   if ! type vim &> /dev/null || ! type dconf &> /dev/null || ! type nvim &> /dev/null; then
-    echo "Instalando dependências"
+    echo "Digite a senha e aguarde a instalação das dependências"
     sudo apt-get install -y $(< apt_packages.txt) &> /dev/null
   else
     printf "%s ~ vim e dconf-cli e nvim já estão instalados%s\n" "$YELLOW" "$NORMAL"
@@ -47,7 +47,7 @@ elif  [[ $(cat /etc/issue) == *"Ubuntu"* ]]; then
 
 elif [[ $(cat /etc/issue) == *"elementary"* ]]; then
   if ! type vim &> /dev/null || ! type nvim &> /dev/null; then
-    echo "Instalando dependências"
+    echo "Digite a senha e aguarde a instalação das dependências"
     sudo apt-get install -y $(< apt_packages.txt) &> /dev/null
   else
     printf "%s ~ vim e dconf-cli e nvim já estão instalados%s\n" "$YELLOW" "$NORMAL"
@@ -72,7 +72,7 @@ elif [[ $(cat /etc/issue) == *"elementary"* ]]; then
 
 elif [[ $(cat /etc/redhat-release) == *"Fedora"* ]]; then
   if ! type vim &> /dev/null || ! type dconf &> /dev/null || ! type nvim &> /dev/null; then
-    echo "Instalando dependências"
+    echo "Digite a senha e aguarde a instalação das dependências"
     sudo dnf install -y $(< dnf_packages.txt) &> /dev/null
   else
     printf "%s ~ vim e dconf-cli e nvim já estão instalados%s\n" "$YELLOW" "$NORMAL"
@@ -80,7 +80,7 @@ elif [[ $(cat /etc/redhat-release) == *"Fedora"* ]]; then
 
 elif [[ $(cat /etc/redhat-release) == *"Red Hat Enterprise Linux"* ]]; then
   if ! type vim &> /dev/null || ! type dconf &> /dev/null || ! type nvim &> /dev/null; then
-    echo "Instalando dependências"
+    echo "Digite a senha e aguarde a instalação das dependências"
     su -c "xargs -a dnf_rhel_packages.txt dnf install -y && \
            curl -o /usr/local/bin/nvim -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage && \
            chmod +x /usr/local/bin/nvim" &> /dev/null
